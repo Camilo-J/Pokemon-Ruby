9004c4dbf99b0a0999ee16610c4194f65332466e
Camilo-J <josecamilo1902@outlook.com>
2022-08-19 20:52:12 -0500

    Add Class leader

--- player.rb
+++ player.rb
@@ -1,14 +1,14 @@
 # require neccesary files
 require_relative "pokemon"
-require_relative "pokedex/pokemons"
 class Player
+  attr_reader :pokemon
   # (Complete parameters)
 
-  def initialize(name, pokemon_species, name_pokemon = nil, level_pokemon = nil)
+  def initialize(name, pokemon_species, name_pokemon, level_pokemon = nil)
     # Complete this
     @name = name
     @level_pokemon = level_pokemon || 1
-    @pokemon = Pokemon.new(pokemon_species.downcase, name_pokemon, @level_pokemon)
+    @pokemon = Pokemon.new(pokemon_species, name_pokemon, @level_pokemon)
   end
 
   def select_move
@@ -21,9 +21,13 @@ class Bot < Player
   def initialize
     pokemon_options = Pokedex::POKEMONS.keys
     selected_pokemon = pokemon_options.sample # ["Bulbasaur", "Charmander", "Squirtle", "Ratata", "Spearow", "Pikachu", "Onix"]
-    super("Random Person", selected_pokemon, selected_pokemon.capitalize, level_pokemon)
+    super("Random Person", selected_pokemon, selected_pokemon.capitalize, rand(1..5))
   end
 end
 
-hola = Player.new("Camilo", "Fuego", "Gotin", 1)
-p hola
+class Leader < Player
+  def initialize
+    super("Gym Leader Brock", "Onix", "Onix", 10)
+  end
+end
+# p playyer1 = Player.new("hola","charmander","Charmander")
