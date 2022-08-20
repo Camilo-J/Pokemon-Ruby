425235714bdc907c391197c0bd77807c438cbe90
Camilo-J <josecamilo1902@outlook.com>
2022-08-19 20:47:13 -0500

    Add variables pokemon class

--- pokemon.rb
+++ pokemon.rb
@@ -1,21 +1,19 @@
 # require neccesary files
 require_relative "pokedex/pokemons"
-require_relative "player"
-require_relative "battle"
-require_relative "game"
+# require_relative "battle"
+# require_relative "game"
 
 class Pokemon
-  attr_reader :char_pokemon , :indivudal_stats, :effort_values
+  attr_reader :char_pokemon , :indivudal_stats, :effort_values, :stat, :level
   # include neccesary modules
 
   # (complete parameters)
   def initialize(specie, name, level)
-    #@char_pokemon = Pokedex::POKEMONS.select { |name, data| name == specie}.values
     @char_pokemon = Pokedex::POKEMONS[specie.capitalize]
     # Retrieve pokemon info from Pokedex and set instance variables
     @name = name || specie
-    @species = specie
-    @level = level || 1
+    @species = specie.capitalize
+     @level = level || 1
     @type = @char_pokemon[:type]
     @base_exp = @char_pokemon[:base_exp]
     @effort_points = @char_pokemon[:effort_points]
@@ -23,26 +21,42 @@ class Pokemon
     @base_stats = @char_pokemon[:base_stats]
     @moves = @char_pokemon[:moves]
     @max_hp = @base_stats[:hp]
-    @hp = @max_hp
-    @attack = @base_stats[:attack]
-    @defense = @base_stats[:defense]
-    @special_attack = @base_stats[:special_attack]
-    @special_defense = @base_stats[:special_defense]
-    @speed = @base_stats[:speed]
     # Calculate Individual Values and store them in instance variable
     @indivudal_stats = { hp: rand(0..31), attack: rand(0..31), defense: rand(0..31), special_attack: rand(0..31), special_defense: rand(0..31), speed: rand(0..31) }
     # Create instance variable with effort values. All set to 0
-    @effort_values = { hp: 0, attack: 0, defense: 0, special_attack: 0, special_defense: 0, speed: 0 }
+    @effort_values = { hp: 5, attack: 2, defense: 20, special_attack: 50, special_defense: 30, speed: 10 }
     @experience_got = 0
+    @current_hp = 0
+    @current_attack = 0
+    @current_defense = 0
+    @current_special_attack = 0
+    @current_special_defense = 0
+    @current_speed = 0
     # Store the level in instance variable
     # If level is 1, set experience points to 0 in instance variable.
    
     # If level is not 1, calculate the minimum experience point for that level and store it in instance variable.
     # Calculate pokemon stats and store them in instance variable
+    @stat = {
+      hp: (((2 * @base_stats[:hp]+ @indivudal_stats[:hp] + (@effort_values[:hp]/4.0).floor) * @level / 100 + level + 10).floor),
+      attack: ((((2 * @base_stats[:attack] + @indivudal_stats[:attack] + (@effort_values[:attack]/4.0).floor) * level / 100) + 5).floor),
+      defense:  (((2 * @base_stats[:defense] + @indivudal_stats[:defense] + (@effort_values[:defense]/4.0).floor) * level / 100 + 5).floor),
+      special_attack:  (((2 * @base_stats[:special_attack] + @indivudal_stats[:special_attack] + (@effort_values[:special_attack]/4.0).floor) * level / 100 + 5).floor),
+      special_defense:  (((2 * @base_stats[:special_defense] + @indivudal_stats[:special_defense] + (@effort_values[:special_defense]/4.0).floor) * level / 100 + 5).floor),
+      speed:  (((2 * @base_stats[:speed] + @indivudal_stats[:speed] + (@effort_values[:speed]/4.0).floor) * level / 100 + 5).floor),
+    }
+    
+  
   end
 
   def prepare_for_battle
     # Complete this
+    @current_hp = @stat[:hp]
+    @current_attack = @stat[:attack]
+    @current_defense = @stat[:defense]
+    @current_special_attack = @stat[:special_attack]
+    @current_special_defense = @stat[:special_defense]
+    @current_speed = @stat[:speed]
   end
 
   def receive_damage
@@ -55,6 +69,11 @@ class Pokemon
 
   def fainted?
     # Complete this
+   if  @current_hp <= 0  
+    true
+   else
+     false
+   end
   end
 
   def attack(target)
@@ -72,12 +91,14 @@ class Pokemon
     # -- Inflict damage to target and print message "And it hit [target name] with [damage] damage""
     # Else, print "But it MISSED!"
   end
-
+  
   def increase_stats(target)
     # Increase stats base on the defeated pokemon and print message "#[pokemon name] gained [amount] experience points"
 
     # If the new experience point are enough to level up, do it and print
     # message "#[pokemon name] reached level [level]!" # -- Re-calculate the stat
+      
+   
   end
 
   # private methods:
@@ -85,8 +106,8 @@ class Pokemon
 end
 
 
-# pokemon1 = Pokemon.new("Charmander", nil, 1)
-# #p pokemon1
-#  p pokemon1.effort_values
-
+#  pokemon1 = Pokemon.new("Charmander", nil, 1)
+# #  #pokemon1
+#  pokemon1.prepare_for_battle
+# p pokemon1.fainted?
 
