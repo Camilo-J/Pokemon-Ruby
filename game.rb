55bf958ac74410c01e5ffd4c0017226a2485257c
Camilo-J <josecamilo1902@outlook.com>
2022-09-22 09:45:56 -0500

    ADD method get_pokemon_name and fix bugs in method game_flow

--- game.rb
+++ game.rb
@@ -5,12 +5,14 @@ require_relative "player"
  include MethodsGame
 class Game
   attr_accessor :name, :pokemon_name, :pokemon, :playyer1
+  
   def initialize
     @name = ""
     @pokemon = ""
     @pokemon_name = ""
     @playyer1 = ""
   end
+
   def start
     #Create a welcome method(s) to get the name, pokemon and pokemon_name from the user
     puts welcome
@@ -19,40 +21,30 @@ class Game
     # puts "1. Charmander        2. Bulbasaur      3. Squirtle"
     starters = Pokedex::POKEMONS.select { |_name, data| data[:starter] == true }
     valid_starter = starters.keys
-    #valid_starter.each.with_index Do |pokemon, index|
-    # print "#{index + 1}. #{pokemon}  "
-    #end
+
     pokemon = get_with_options2("Choose a starting pokemon",valid_starter)
-    puts "You selected #{pokemon} Great choice!
-    Give your pokemon a name?"
-    pokemon_name = gets.chomp
-    pokemon_name = pokemon  if @pokemon_name.empty?
-    puts "#{name}, raise your young #{pokemon_name} by making it fight!
-    When you feel ready you can challenge BROCK, the PEWTER's GYM LEADER"
-    
-    playyer1 = Player.new(name,pokemon,pokemon_name)
+    pokemon_name = get_pokemon_name(name,pokemon)
     # create_player
+    playyer1 = Player.new(name,pokemon,pokemon_name)
     game_flow(playyer1)
-    goodbye
   end
 
   def train(playyer1,bot)
     # Complete this
-    #bot = Bot.new
     battle_bot = Battle.new(playyer1, bot )
     battle_bot.start
   end
 
   def challenge_leader(playyer1,leader)
     # Complete this
-    #leader = Leader.new
     battle_leader = Battle.new(playyer1, leader )
     battle_leader.start
   end
 
-  def show_stats(jugador,pokemon_name_gotten)
+  def show_stats(jugador,pokemon_name)
     # Complete this
-   puts ["#{pokemon_name_gotten}:",
+    p pokemon_name
+   puts ["#{pokemon_name}:",
     "Kind: #{jugador.pokemon.species}",
     "Level: #{jugador.pokemon.level}",
     "Type: #{jugador.pokemon.type}",
@@ -70,32 +62,23 @@ class Game
 
   def game_flow(playyer1)
     options = ["fight", "leave"]
-    option_menu = ["Train", "Leader", "Stats"]
+    option_menu = ["Train", "Leader", "Stats","Exit"]
     action = ""
     until action == "Exit"
       action = print_menu
       case action
       when "Train"
-      bot = Bot.new
-      valor = desicion(options, playyer1, bot)
-      if valor.downcase == "fight"
-        train(playyer1,bot)
-        action = print_menu
-      else
-        action = print_menu
-      end
+        bot = Bot.new
+        valor = desicion(options, playyer1, bot)
+        train(playyer1,bot) if valor.downcase == "fight"
       when "Leader"
-      leader = Leader.new
-      valor1 = desicion(options,playyer1,leader,"fight!")
-      if valor1.downcase == "fight"
-        challenge_leader(playyer1,leader)
-        action = print_menu
-      else
-        action = print_menu
-      end
+        leader = Leader.new
+        valor1 = desicion(options,playyer1,leader,"fight!")
+        challenge_leader(playyer1,leader) if valor1.downcase == "fight"
       when "Stats"
-      show_stats(playyer1,pokemon_name_gotten)
-      action = print_menu
+        show_stats(playyer1,pokemon_name)
+      when "Exit"
+        puts goodbye
       end
       puts "Invalid Option" unless option_menu.include?(action)
     end
