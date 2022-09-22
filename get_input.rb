88ba10b189642a3e0e8397f0058500d4a7064d06
Camilo-J <josecamilo1902@outlook.com>
2022-09-21 19:22:41 -0500

    Add new module MethodsGame and method print_menu

--- get_input.rb
+++ get_input.rb
@@ -42,45 +42,57 @@ module GetInput
       input
     end
   end
-def welcome
-   ["#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#",
-    "#$#$#$#$#$#$#$                               $#$#$#$#$#$#$#",
-    "#$##$##$##$ ---        Pokemon Ruby         --- #$##$##$#$#",
-    "#$#$#$#$#$#$#$                               $#$#$#$#$#$#$#",
-    "#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#",
-    "Hello there! Welcome to the world of POKEMON! My name is OAK!",
-    "People call me the POKEMON PROF!",
-    "This world is inhabited by creatures called POKEMON! For some people",
-    "POKEMON are pets. Others use them For fights. Myself...",
-    "I study POKEMON as a profession."]
-end
 
-def welcome_with_data(name)
-  ["Right! So your name is #{name}!",
-    "Your very own POKEMON legend is about to unfold! A world of",
-    "dreams and adventures with POKEMON awaits! Lets go!",
-    "Here, #{name}! There are 3 POKEMON here! Haha!",
-    "When I was young, I was a serious POKEMON trainer.",
-    "In my old age, I have only 3 left, but you can have one! Choose!\n\n"
-  ]
-end
+  module MethodsGame
+    def welcome
+      ["#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#",
+       "#$#$#$#$#$#$#$                               $#$#$#$#$#$#$#",
+       "#$##$##$##$ ---        Pokemon Ruby         --- #$##$##$#$#",
+       "#$#$#$#$#$#$#$                               $#$#$#$#$#$#$#",
+       "#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#",
+       "Hello there! Welcome to the world of POKEMON! My name is OAK!",
+       "People call me the POKEMON PROF!",
+       "This world is inhabited by creatures called POKEMON! For some people",
+       "POKEMON are pets. Others use them For fights. Myself...",
+       "I study POKEMON as a profession."]
+   end
+   
+   def welcome_with_data(name)
+     ["Right! So your name is #{name}!",
+       "Your very own POKEMON legend is about to unfold! A world of",
+       "dreams and adventures with POKEMON awaits! Lets go!",
+       "Here, #{name}! There are 3 POKEMON here! Haha!",
+       "When I was young, I was a serious POKEMON trainer.",
+       "In my old age, I have only 3 left, but you can have one! Choose!\n\n"
+     ]
+   end
+   
+   def get_with_options2(prompt, options)
+     input = ""
+     until options.include?(input)
+       puts prompt
+       print_options2(options)
+       print "> "
+       input = gets.chomp
+       puts "Invalid option" unless options.include?(input)
+     end
+   
+     input
+   end
+   
+   def print_options2(options)
+     options.each.with_index do |option, index|
+       print "#{index + 1}. #{option.capitalize}   "
+     end
+     puts ""
+   end
 
-def get_with_options2(prompt, options)
-  input = ""
-  until options.include?(input)
-    puts prompt
-    print_options2(options)
+   def print_menu
+    puts "\n" + "-" * 30 + "Menu" + "-" * 30
+    puts "1. Stats           2. Train          3. Leader"+"          4. Exit"
+    puts "\n"
+  
     print "> "
-    input = gets.chomp
-    puts "Invalid option" unless options.include?(input)
+    gets.chomp.strip.capitalize
   end
-
-  input
-end
-
-def print_options2(options)
-  options.each.with_index do |option, index|
-    print "#{index + 1}. #{option.capitalize}   "
-  end
-  puts ""
 end
