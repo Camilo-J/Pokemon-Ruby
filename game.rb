a3928d2d28b7b4597bdb34f41cfad98ba0987f08
Camilo-J <josecamilo1902@outlook.com>
2022-12-29 10:21:34 -0500

    Fix bugs & Refactor code

--- game.rb
+++ game.rb
@@ -1,11 +1,12 @@
-#require neccesary files
+# require neccesary files
 require_relative "player"
 # require_relative "battle"
- include GetInput
- include MethodsGame
 class Game
+  include GetInput
+  include MethodsGame
+
   attr_accessor :name, :pokemon_name, :pokemon, :playyer1
-  
+
   def initialize
     @name = ""
     @pokemon = ""
@@ -14,7 +15,7 @@ class Game
   end
 
   def start
-    #Create a welcome method(s) to get the name, pokemon and pokemon_name from the user
+    # Create a welcome method(s) to get the name, pokemon and pokemon_name from the user
     puts welcome
     name = get_input("First, what is your name?")
     puts welcome_with_data(name)
@@ -22,27 +23,27 @@ class Game
     starters = Pokedex::POKEMONS.select { |_name, data| data[:starter] == true }
     valid_starter = starters.keys
 
-    pokemon = get_with_options2("Choose a starting pokemon",valid_starter)
-    pokemon_name = get_pokemon_name(name,pokemon)
+    pokemon = get_with_options2("Choose a starting pokemon", valid_starter)
+    pokemon_name = get_pokemon_name(name, pokemon)
     # create_player
-    playyer1 = Player.new(name,pokemon,pokemon_name)
-    game_flow(playyer1,pokemon_name)
+    playyer1 = Player.new(name, pokemon, pokemon_name)
+    game_flow(playyer1, pokemon_name)
   end
 
-  def train(playyer1,bot)
+  def train(playyer1, bot)
     # Complete this
-    battle_bot = Battle.new(playyer1, bot )
+    battle_bot = Battle.new(playyer1, bot)
     battle_bot.start
   end
 
-  def challenge_leader(playyer1,leader)
+  def challenge_leader(playyer1, leader)
     # Complete this
-    battle_leader = Battle.new(playyer1, leader )
+    battle_leader = Battle.new(playyer1, leader)
     battle_leader.start
   end
 
-  def game_flow(playyer1,pokemon_name)
-    options = [["fight", "leave"],["Train", "Leader", "Stats","Exit"]]
+  def game_flow(playyer1, pokemon_name)
+    options = [["fight", "leave"], ["Train", "Leader", "Stats", "Exit"]]
     action = ""
     until action == "Exit"
       action = print_menu
@@ -50,13 +51,13 @@ class Game
       when "Train"
         bot = Bot.new
         valor = desicion(options[0], playyer1, bot)
-        train(playyer1,bot) if valor.downcase == "fight"
+        train(playyer1, bot) if valor.downcase == "fight"
       when "Leader"
         leader = Leader.new
-        valor1 = desicion(options[0],playyer1,leader,"fight!")
-        challenge_leader(playyer1,leader) if valor1.downcase == "fight"
+        valor1 = desicion(options[0], playyer1, leader, "fight!")
+        challenge_leader(playyer1, leader) if valor1.downcase == "fight"
       when "Stats"
-        show_stats(playyer1,pokemon_name)
+        show_stats(playyer1, pokemon_name)
       when "Exit"
         puts goodbye
       end
--- pokemon.rb
+++ pokemon.rb
@@ -5,9 +5,8 @@ require_relative "pokedex/moves"
 # require_relative "game"
 
 class Pokemon
-  attr_reader :char_pokemon , :indivudal_stats, :species, :type, :moves, :name, :effort_points, :experience_got
+  attr_reader :char_pokemon, :indivudal_stats, :species, :type, :moves, :name, :effort_points, :experience_got
   attr_accessor :current_move, :current_hp, :current_speed, :base_exp, :level, :effort_values, :stat
-  # include neccesary modules
 
   # (complete parameters)
   def initialize(specie, name, level)
@@ -24,7 +23,8 @@ class Pokemon
     @moves = @char_pokemon[:moves]
     @max_hp = @base_stats[:hp]
     # Calculate Individual Values and store them in instance variable
-    @indivudal_stats = { hp: rand(0..31), attack: rand(0..31), defense: rand(0..31), special_attack: rand(0..31), special_defense: rand(0..31), speed: rand(0..31) }
+    @indivudal_stats = { hp: rand(0..31), attack: rand(0..31), defense: rand(0..31), special_attack: rand(0..31),
+                         special_defense: rand(0..31), speed: rand(0..31) }
     # Create instance variable with effort values. All set to 0
     @effort_values = { hp: 0, attack: 0, defense: 0, special_attack: 0, special_defense: 0, speed: 0 }
     @current_exp = 0
@@ -39,19 +39,17 @@ class Pokemon
     @current_move = nil
     # Store the level in instance variable
     # If level is 1, set experience points to 0 in instance variable.
-   
+
     # If level is not 1, calculate the minimum experience point for that level and store it in instance variable.
     # Calculate pokemon stats and store them in instance variable
     @stat = {
-      hp: (((2 * @base_stats[:hp]+ @indivudal_stats[:hp] + (@effort_values[:hp]/4.0).floor) * @level / 100 + level + 10).floor),
-      attack: ((((2 * @base_stats[:attack] + @indivudal_stats[:attack] + (@effort_values[:attack]/4.0).floor) * level / 100) + 5).floor),
-      defense:  (((2 * @base_stats[:defense] + @indivudal_stats[:defense] + (@effort_values[:defense]/4.0).floor) * level / 100 + 5).floor),
-      special_attack:  (((2 * @base_stats[:special_attack] + @indivudal_stats[:special_attack] + (@effort_values[:special_attack]/4.0).floor) * level / 100 + 5).floor),
-      special_defense:  (((2 * @base_stats[:special_defense] + @indivudal_stats[:special_defense] + (@effort_values[:special_defense]/4.0).floor) * level / 100 + 5).floor),
-      speed:  (((2 * @base_stats[:speed] + @indivudal_stats[:speed] + (@effort_values[:speed]/4.0).floor) * level / 100 + 5).floor),
+      hp: ((((2 * @base_stats[:hp]) + @indivudal_stats[:hp] + (@effort_values[:hp] / 4.0).floor) * @level / 100) + level + 10).floor,
+      attack: ((((2 * @base_stats[:attack]) + @indivudal_stats[:attack] + (@effort_values[:attack] / 4.0).floor) * level / 100) + 5).floor,
+      defense: ((((2 * @base_stats[:defense]) + @indivudal_stats[:defense] + (@effort_values[:defense] / 4.0).floor) * level / 100) + 5).floor,
+      special_attack: ((((2 * @base_stats[:special_attack]) + @indivudal_stats[:special_attack] + (@effort_values[:special_attack] / 4.0).floor) * level / 100) + 5).floor,
+      special_defense: ((((2 * @base_stats[:special_defense]) + @indivudal_stats[:special_defense] + (@effort_values[:special_defense] / 4.0).floor) * level / 100) + 5).floor,
+      speed: ((((2 * @base_stats[:speed]) + @indivudal_stats[:speed] + (@effort_values[:speed] / 4.0).floor) * level / 100) + 5).floor
     }
-    
-  
   end
 
   def prepare_for_battle
@@ -70,38 +68,37 @@ class Pokemon
 
   def fainted?
     # Complete this
-   if  @current_hp <= 0  
-    true
-   else
-     false
-   end
+    @current_hp <= 0
   end
 
   def attack(target)
     # Print attack message 'Tortuguita used MOVE!'
-    puts "#{self.name} used #{self.current_move[:name]}!"
+    puts "#{name} used #{current_move[:name]}!"
     # Accuracy check
     hits = @current_move[:accuracy] >= rand(1..100)
     # If the movement is not missed
     # -- Calculate base damage
-    
+
     # -- Mutltiply damage by effectiveness multiplier and round down. Print message If neccesary
     # ---- "It's not very effective..." when effectivenes is less than or equal to 0.5
     # ---- "It's super effective!" when effectivenes is greater than or equal to 1.5
     # ---- "It doesn't affect [target name]!" when effectivenes is 0
     # -- Inflict damage to target and print message "And it hit [target name] with [damage] damage""
     if hits
-      base_damage =(((2 * self.level / 5.0 + 2).floor * offensive_stat(self.current_move[:type], target.type) * @current_move[:power] / target_defensive_stat(self.current_move[:type],target)).floor / 50.0).floor + 2
+      base_damage = ((((2 * level / 5.0) + 2).floor * offensive_stat(current_move[:type],
+                                                                     target.type) * @current_move[:power] / target_defensive_stat(
+                                                                       current_move[:type], target
+                                                                     )).floor / 50.0).floor + 2
       # -- Critical Hit check
       critical_hit = 16 == rand(1..16)
       # -- If critical, multiply base damage and print message 'It was CRITICAL hit!'
       if critical_hit
-         current_damage = base_damage * 1.5
+        current_damage = base_damage * 1.5
         puts "It was CRITICAL hit"
       else
         current_damage = base_damage
       end
-      multi = offensive_stat(self.current_move[:type], target.type)
+      multi = offensive_stat(current_move[:type], target.type)
       # -- Effectiveness check
       if multi == 0.5
         current_damage *= 0.5
@@ -109,7 +106,7 @@ class Pokemon
       elsif multi == 2
         current_damage *= 2
         puts "It's super effective!"
-      elsif multi == 0
+      elsif multi.zero?
         current_damage *= 0
         puts "It doesn't affect #{target.name}!"
       else
@@ -122,61 +119,52 @@ class Pokemon
     end
     # Else, print "But it MISSED!"
   end
-  
+
   def increase_stats(target)
     # Increase stats base on the defeated pokemon and print message "#[pokemon name] gained [amount] experience points"
-    @exp_tolevelup = (6 / 5.0 * level**3 - 15 * level**2 + 100 * level - 140).floor
+    @exp_tolevelup = ((6 / 5.0 * (level**3)) - (15 * (level**2)) + (100 * level) - 140).floor
     # If the new experience point are enough to level up, do it and print
     @experience_got = (target.base_exp * level / 7.0).floor
     @current_exp += @experience_got
     # message "#[pokemon name] reached level [level]!" # -- Re-calculate the stat
-      if @current_exp >= @exp_tolevelup
-        @level += 1
-        calculate_effortValues(target)
-        @stat = {
-          hp: (((2 * @base_stats[:hp]+ @indivudal_stats[:hp] + (@effort_values[:hp]/4.0).floor) * @level / 100 + level + 10).floor),
-          attack: ((((2 * @base_stats[:attack] + @indivudal_stats[:attack] + (@effort_values[:attack]/4.0).floor) * level / 100) + 5).floor),
-          defense:  (((2 * @base_stats[:defense] + @indivudal_stats[:defense] + (@effort_values[:defense]/4.0).floor) * level / 100 + 5).floor),
-          special_attack:  (((2 * @base_stats[:special_attack] + @indivudal_stats[:special_attack] + (@effort_values[:special_attack]/4.0).floor) * level / 100 + 5).floor),
-          special_defense:  (((2 * @base_stats[:special_defense] + @indivudal_stats[:special_defense] + (@effort_values[:special_defense]/4.0).floor) * level / 100 + 5).floor),
-          speed:  (((2 * @base_stats[:speed] + @indivudal_stats[:speed] + (@effort_values[:speed]/4.0).floor) * level / 100 + 5).floor),
-        }
-      end
+    if @current_exp >= @exp_tolevelup
+      @level += 1
+      calculate_effort_values(target)
+      @stat = {
+        hp: ((((2 * @base_stats[:hp]) + @indivudal_stats[:hp] + (@effort_values[:hp] / 4.0).floor) * @level / 100) + level + 10).floor,
+        attack: ((((2 * @base_stats[:attack]) + @indivudal_stats[:attack] + (@effort_values[:attack] / 4.0).floor) * level / 100) + 5).floor,
+        defense: ((((2 * @base_stats[:defense]) + @indivudal_stats[:defense] + (@effort_values[:defense] / 4.0).floor) * level / 100) + 5).floor,
+        special_attack: ((((2 * @base_stats[:special_attack]) + @indivudal_stats[:special_attack] + (@effort_values[:special_attack] / 4.0).floor) * level / 100) + 5).floor,
+        special_defense: ((((2 * @base_stats[:special_defense]) + @indivudal_stats[:special_defense] + (@effort_values[:special_defense] / 4.0).floor) * level / 100) + 5).floor,
+        speed: ((((2 * @base_stats[:speed]) + @indivudal_stats[:speed] + (@effort_values[:speed] / 4.0).floor) * level / 100) + 5).floor
+      }
+    end
   end
-  
+
   # private methods: vtarget_defensive_stat
   private
+
   # Create here auxiliary methods
-  def offensive_stat(type_attack,enemy_type)
+  def offensive_stat(type_attack, enemy_type)
     types_multipler = Pokedex::TYPE_MULTIPLIER
     multiplier_damage = 1
-    types_multipler.each do |moves| 
-      if moves[:user] == type_attack &&  moves[:target] == enemy_type[0]
-        multiplier_damage = moves[:multiplier]
-      end
+    types_multipler.each do |moves|
+      multiplier_damage = moves[:multiplier] if moves[:user] == type_attack && moves[:target] == enemy_type[0]
     end
-   multiplier_damage #[0.5]
+    multiplier_damage # [0.5]
   end
 
-  def target_defensive_stat(type_attack,enemy)
-   if type_attack == :normal
-      enemy.stat[:defense]
-   else
-    enemy.stat[:special_defense]
-    end
-end
-
-def calculate_effortValues(enemy)
-  effort_po = enemy.effort_points
-  effort_va = self.effort_values
-
-  effort_va.each do |key, value| 
-    if key == effort_po[:type] 
-       value += effort_po[:amount]
-    end
+  def target_defensive_stat(type_attack, enemy)
+    type_attack == :normal ? enemy.stat[:defense] : enemy.stat[:special_defense]
   end
-  self.effort_values = effort_va
-end
 
-end
+  def calculate_effort_values(enemy)
+    effort_po = enemy.effort_points
+    effort_va = effort_values
 
+    effort_va.each do |key, value|
+      value += effort_po[:amount] if key == effort_po[:type]
+    end
+    self.effort_values = effort_va
+  end
+end
